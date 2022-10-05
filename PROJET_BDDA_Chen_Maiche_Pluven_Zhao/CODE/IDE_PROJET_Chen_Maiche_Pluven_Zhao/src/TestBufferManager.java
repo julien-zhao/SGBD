20d70d2c2914441751ebe56435998eea33c8857a
Julien ZHAO <julien@MBP-de-Julien.lan>
2022-10-05 23:17:44 +0200

    début testBufferManager

--- PROJET_BDDA_Chen_Maiche_Pluven_Zhao/CODE/IDE_PROJET_Chen_Maiche_Pluven_Zhao/src/TestBufferManager.java
+++ PROJET_BDDA_Chen_Maiche_Pluven_Zhao/CODE/IDE_PROJET_Chen_Maiche_Pluven_Zhao/src/TestBufferManager.java
@@ -15,6 +15,28 @@ public class TestBufferManager {
 		System.out.println("Test DeallocPage : "); 
 		PageId p1= DiskManager.allocPage();
 		PageId p2= DiskManager.allocPage();
+		byte[] bb = "hello".getBytes();
+		dm.writePage(p1, bb);
+		dm.writePage(p2, bb);
+		dm.readPage(p1, bb);
+		dm.readPage(p2, bb);
+		System.out.println("=============================");
+		
+		System.out.println("\n=============================");
+		System.out.println("Test de GetPage");
+		//bm.getPage(p1);
+		System.out.println("=============================");
+		
+		
+		System.out.println("\n=============================");
+		System.out.println("Test de FreePage");
+		//bm.freePage(p1, true);
+		System.out.println("=============================");
+		
+		System.out.println("\n=============================");
+		System.out.println("Test de FlushAll");
+		//bm.flushAll();
+		System.out.println("=============================");
 		
 	}
 }
