402afbde68cde050f44bacccaf8d7e45f24a0d1d
CHEN LOUIS <il08326@pc523a2-1.ens.math-info.univ-paris5.fr>
2022-10-06 15:46:32 +0200

    class relation info

--- PROJET_BDDA_Chen_Maiche_Pluven_Zhao/CODE/IDE_PROJET_Chen_Maiche_Pluven_Zhao/src/RelationInfo.java
+++ PROJET_BDDA_Chen_Maiche_Pluven_Zhao/CODE/IDE_PROJET_Chen_Maiche_Pluven_Zhao/src/RelationInfo.java
@@ -1,11 +1,44 @@
 
 public class RelationInfo {
-	String nomRelation;
-	int nbColonne;
-	int nomColonne;
-	int typeColonne;
+	private String nomRelation; // le nom de la relation
+	private int nbColonnes;		// le nombre de colonnes
+	//private String[] nomColonnes;	// les noms des colonnes
+	//private int[] typesColonnes;	// les types des colonnes
+	/*
+	 *  Stocker les noms des colonnes en tant que liste ou tableau
+	 *  de caractères, et faire de même pour les types des colonnes
+	 *
+	 *
+	 * il y a trois types de colonnes possibles dans ce projet:
+	 * 	- INTEGER correspond à un type entier sur 4 octets
+	 *  - REAL correspond à un type float sur 4 octets
+	 *  - VARCHAR(T) correspond à une chaîne de caractères de 
+	 *  taille maximale (nombre maximal de caractères) T
+	 */
+	
+	
+	public RelationInfo(String nomRelation,int nbColonnes) {
+		this.nomRelation = nomRelation;
+		this.nbColonnes = nbColonnes;
+	}
 	
 	public String getNomRelation() {
 		return nomRelation;
+	}
+	
+	public void setNomRelation(String nomRelation) {
+		this.nomRelation = nomRelation;
+	}
+	
+	public int getNbColonnes(){
+		return nbColonnes;
+	}
+	
+	public void setNbColonnes(int nbColonnes) {
+		this.nbColonnes = nbColonnes;
+	}
+	
+	public String toString() {
+		return "Le nomRelation: "+this.getNomRelation()+"\nNombre de Colonnes:"+this.getNbColonnes();
 	}
 }
