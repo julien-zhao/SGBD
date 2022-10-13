ddada429804fddb74a4ee4fbf2dabcdead88e8a1
Moumix <max.maiche@gmail.com>
2022-10-13 14:22:46 +0200

    FileManager++

--- PROJET_BDDA_Chen_Maiche_Pluven_Zhao/CODE/IDE_PROJET_Chen_Maiche_Pluven_Zhao/src/FileManager.java
+++ PROJET_BDDA_Chen_Maiche_Pluven_Zhao/CODE/IDE_PROJET_Chen_Maiche_Pluven_Zhao/src/FileManager.java
@@ -1,14 +1,38 @@
+import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public class FileManager {
 	private static FileManager g_instance = new FileManager();
-	
+	private PageId pIdHeader;
 	private FileManager() {
-		
+		try {
+		pIdHeader = createNewHeaderPage();
+	
+		}catch(IOException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	
 	public FileManager getSingleton() {
 		return g_instance;
 	}
 	
-	
+
+	public PageId createNewHeaderPage() throws IOException {
+		PageId p=DiskManager.allocPage();
+		
+		BufferManager bm = BufferManager.getSingleton();
+		
+		byte[] zerobin = ByteBuffer.allocate(4).putInt(0).array();
+		byte[] bb = bm.getPage(p);
+		
+		for(int i = 0;i<32;i++) {
+			bb[i] = zerobin[i];
+		}
+		
+		bm.freePage(p, true);
+		return p;
+	}
 }
+
+
