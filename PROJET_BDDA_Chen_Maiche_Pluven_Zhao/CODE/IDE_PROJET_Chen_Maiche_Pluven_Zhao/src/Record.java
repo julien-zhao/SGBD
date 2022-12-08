164a7e764f27ea3bb9689ed2a9a7060a5aa21e12
ZHAO JULIEN <im00018@migre-2.ens.math-info.univ-paris5.fr>
2022-12-08 15:23:11 +0100

    debug readBuffer tp4

--- PROJET_BDDA_Chen_Maiche_Pluven_Zhao/CODE/IDE_PROJET_Chen_Maiche_Pluven_Zhao/src/Record.java
+++ PROJET_BDDA_Chen_Maiche_Pluven_Zhao/CODE/IDE_PROJET_Chen_Maiche_Pluven_Zhao/src/Record.java
@@ -6,189 +6,178 @@ public class Record {
 
 	private RelationInfo relInfo;
 	private ArrayList<String> values;
-	private int bufferSize=0;
+	private int bufferSize = 0;
 
-
-	
 	public Record(RelationInfo relInfo) {
 		this.relInfo = relInfo;
 		values = new ArrayList<String>();
 	}
-	
+
 	public RelationInfo getRelInfo() {
 		return relInfo;
 	}
-	
-	public void setValues(String... s){
+
+	public void setValues(String... s) {
 		values.clear();
-		for(String str : s){
+		for (String str : s) {
 			values.add(str);
 		}
 	}
 
-
-
 	public void addTuple(List<String> unTuple) {
-		//pour chaque colone, 
-		bufferSize = unTuple.size()*4;
+		// pour chaque colone,
+		bufferSize = unTuple.size() * 4;
 
-		if(values.size() == 0) {
-			for(int i =0; i<unTuple.size();i++) {
-				//tester si le type d'entrer est le meme que le type des colonnes
-				if ( relInfo.getTabInfo().get(i).getType().equals("INTEGER") ){
-					bufferSize+=4;	
+		if (values.size() == 0) {
+			for (int i = 0; i < unTuple.size(); i++) {
+				// tester si le type d'entrer est le meme que le type des colonnes
+				if (relInfo.getTabInfo().get(i).getType().equals("INTEGER")) {
+					bufferSize += 4;
 				}
-				if ( relInfo.getTabInfo().get(i).getType().equals("REAL") ){
-					bufferSize+=4;
+				if (relInfo.getTabInfo().get(i).getType().equals("REAL")) {
+					bufferSize += 4;
 				}
 				values.add(unTuple.get(i));
 			}
-			for (int i=0; i< relInfo.getTabInfo().size();i++) {
-				if ( relInfo.getTabInfo().get(i).getType().contains("VARCHAR(") ){
-					bufferSize+= getValues().get(i).length()*2 ;
+			for (int i = 0; i < relInfo.getTabInfo().size(); i++) {
+				if (relInfo.getTabInfo().get(i).getType().contains("VARCHAR(")) {
+					bufferSize += getValues().get(i).length() * 2;
 				}
 			}
 		}
-		if(unTuple.size() == relInfo.getTabInfo().size()) {
-			for(int i =0; i<unTuple.size();i++) {
-				values.set(i,unTuple.get(i));
+		if (unTuple.size() == relInfo.getTabInfo().size()) {
+			for (int i = 0; i < unTuple.size(); i++) {
+				values.set(i, unTuple.get(i));
 			}
 		}
 	}
-	
-	
+
 	public String afficheValues() {
 		return values.toString();
 	}
-	
-	public ArrayList<String> getValues(){
+
+	public ArrayList<String> getValues() {
 		return values;
 	}
-	
+
 	public ArrayList<String> getNomColonne() {
-		ArrayList<String> res= new ArrayList<>();
-		for(ColInfo uneCol: relInfo.getTabInfo()){
+		ArrayList<String> res = new ArrayList<>();
+		for (ColInfo uneCol : relInfo.getTabInfo()) {
 			res.add(uneCol.getColonne());
 		}
 		return res;
 	}
+
 	public ArrayList<String> getTypeColonne() {
-		ArrayList<String> res= new ArrayList<>();
-		for(ColInfo uneCol: relInfo.getTabInfo()){
+		ArrayList<String> res = new ArrayList<>();
+		for (ColInfo uneCol : relInfo.getTabInfo()) {
 			res.add(uneCol.getType());
 		}
 		return res;
 	}
-	
-	
-	public int getSizePos() {
-		int size = getValues().size();
 
-		for(int i =0; i< getValues().size();i++) {
-			//si c'est un real ou integer
-			if(relInfo.getTabInfo().get(i).getType().equals("REAL") || relInfo.getTabInfo().get(i).getType().equals("INTEGER")) {
-				size+=1;
-			}else {
-				size+= getValues().get(i).length();
+	public void writeToBuffer(ByteBuffer buff, int pos) {
+
+		buff.position(pos);
+
+		// boucle qui met les tailles des valeurs dans le buff
+		for (int i = 0; i < relInfo.getTabInfo().size(); i++) {
+
+			if (relInfo.getTabInfo().get(i).getType().equals("INTEGER")) {
+				// posList.add(4);
+				buff.putInt(4);
 			}
-		}
-		return size;
-	}
-
-	public void writeToBuffer(ByteBuffer buff, int pos){
-
-		buff.position(pos); 
-
-		//boucle qui met les tailles des valeurs dans le buff
-		for (int i=0; i< relInfo.getTabInfo().size();i++) {
-
-			if ( relInfo.getTabInfo().get(i).getType().equals("INTEGER") ){
-				//posList.add(4);
-				buff.putInt(4); 
+			if (relInfo.getTabInfo().get(i).getType().equals("REAL")) {
+				// posList.add(4);
+				buff.putInt(4);
 			}
-			if ( relInfo.getTabInfo().get(i).getType().equals("REAL") ){
-				//posList.add(4);
-				buff.putInt(4);	
-			}
-			if ( relInfo.getTabInfo().get(i).getType().contains("VARCHAR(") ){
-				int sizeString = getValues().get(i).length()*2;
-				//posList.add(sizeString);
+			if (relInfo.getTabInfo().get(i).getType().contains("VARCHAR(")) {
+				int sizeString = getValues().get(i).length() * 2;
+				// posList.add(sizeString);
 				buff.putInt(sizeString);
 			}
 		}
-		
-		//boucle qui transforme les valeurs en byte
-		for (int i=0; i< relInfo.getTabInfo().size();i++) {
-			if ( relInfo.getTabInfo().get(i).getType().equals("INTEGER") ){
+
+		// boucle qui transforme les valeurs en byte
+		for (int i = 0; i < relInfo.getTabInfo().size(); i++) {
+			if (relInfo.getTabInfo().get(i).getType().equals("INTEGER")) {
 				int tmpInt = Integer.parseInt(getValues().get(i));
 				buff.putInt(tmpInt);
 			}
-			if ( relInfo.getTabInfo().get(i).getType().equals("REAL") ){
+			if (relInfo.getTabInfo().get(i).getType().equals("REAL")) {
 				float tmpFloat = Float.parseFloat(getValues().get(i));
-				buff.putFloat(tmpFloat);	
+				buff.putFloat(tmpFloat);
 			}
-			if ( relInfo.getTabInfo().get(i).getType().contains("VARCHAR(") ){
+			if (relInfo.getTabInfo().get(i).getType().contains("VARCHAR(")) {
 				String tmpString = getValues().get(i);
-				
-				for(int j =0; j< tmpString.length();j++) {
+
+				for (int j = 0; j < tmpString.length(); j++) {
 					buff.putChar(tmpString.charAt(j));
 				}
 			}
 		}
 
 	}
-	
-	//Il sert a obtenir le record en format byte
+
+	// Il sert a obtenir le record en format byte
 	public void getBufferToByte(ByteBuffer buff, int pos) {
 		buff.position(0);
-		while(buff.hasRemaining()) {
+		while (buff.hasRemaining()) {
 			bufferSize++;
 			System.out.print(buff.get() + " ");
-		}		
+		}
 	}
-	
+
+	/**
+	 * la methode
+	 * 
+	 * @param buff
+	 * @param pos
+	 */
 	public void readFromBuffer(ByteBuffer buff, int pos) {
 		buff.position(pos);
-		ArrayList<Integer>posList = new ArrayList<>();
+		ArrayList<Integer> posList = new ArrayList<>();
 
-		for(int i =0;i <relInfo.getTabInfo().size(); i++) {
+		for (int i = 0; i < relInfo.getTabInfo().size(); i++) {
 			bufferSize += 4;
 			posList.add(buff.getInt());
-		}	
+		}
 		ArrayList<String> list = getTypeColonne();
-		StringBuilder sb = new StringBuilder("");;
-		for(int i =0; i<posList.size();i++) {
-			if(posList.get(i) > 4) {
-				for(int j =0; j< posList.get(i)/2; j++) {
+		StringBuilder sb = new StringBuilder("");
+
+		for (int i = 0; i < posList.size(); i++) {
+			bufferSize += 4;
+			if (list.get(i).equals("INTEGER")) {
+				sb.append(buff.getInt());
+				values.add(sb.toString());
+				sb = new StringBuilder("");
+			}
+			if (list.get(i).equals("REAL")) {
+				sb.append(buff.getFloat());
+				values.add(sb.toString());
+				sb = new StringBuilder("");
+
+			}
+			if (list.get(i).startsWith("VARCHAR")) {
+				for (int j = 0; j < posList.get(i) / 2; j++) {
 					bufferSize += 2;
 					sb.append(buff.getChar());
 				}
 				values.add(sb.toString());
 				sb = new StringBuilder("");
 			}
-			if(posList.get(i) == 4) {
-				bufferSize += 4;
-				if(list.get(i).equals("INTEGER")) {
-					sb.append(buff.getInt());
-					values.add(sb.toString());
-					sb = new StringBuilder("");
-				}else {
-					sb.append(buff.getFloat());
-					values.add(sb.toString());
-					sb = new StringBuilder("");
-				}
-			}
 		}
 	}
-	
+
 	public String toString() {
 		String res = "";
-		for (String s: values) {
+		for (String s : values) {
 			res += s + " ";
 		}
-		return res; 
-		
+		return res;
+
 	}
+
 	public int getWrittenSize() {
 		return this.bufferSize;
 	}
--- PROJET_BDDA_Chen_Maiche_Pluven_Zhao/CODE/IDE_PROJET_Chen_Maiche_Pluven_Zhao/src/TestTP04.java
+++ PROJET_BDDA_Chen_Maiche_Pluven_Zhao/CODE/IDE_PROJET_Chen_Maiche_Pluven_Zhao/src/TestTP04.java
@@ -1,21 +1,18 @@
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
-
 public class TestTP04 {
-	public static void main(String []args ) throws Exception {
+	public static void main(String[] args) throws Exception {
 		DBParams.DBPath = ".//..//..//DB//";
 		DBParams.pageSize = 4096;
 		DBParams.maxPagesPerFiles = 4;
 		DBParams.frameCount = 2;
-		//DiskManager dm = DiskManager.getSingleton();
-		
-		//mettre une limite pour un record
+		// DiskManager dm = DiskManager.getSingleton();
+
+		// mettre une limite pour un record
 		System.out.println(DBParams.DBPath);
 		System.out.println("Working Directory = " + System.getProperty("user.dir"));
-		
 
-		
 		System.out.println("\nTest Relation Info et ColInfo");
 		System.out.println("***********************************************************");
 		RelationInfo Personne = new RelationInfo("Personne");
@@ -23,18 +20,11 @@ public class TestTP04 {
 		Personne.addColonne("PRENOM", "VARCHAR(12)");
 		Personne.addColonne("AGE", "INTEGER");
 		Personne.addColonne("TAILLE", "REAL");
-	
-		
 
-		
-		System.out.println("Nom de la relation : "+Personne.getNomRelation()+"\n");
-		System.out.println("Affiche toute les relations de Personne: "+Personne.afficheRelationInfo());
+		System.out.println("Nom de la relation : " + Personne.getNomRelation() + "\n");
+		System.out.println("Affiche toute les relations de Personne: " + Personne.afficheRelationInfo());
 		System.out.println("***********************************************************");
-		
-		
-		
-		
-		
+
 		System.out.println("\n\n\nTest Catalog");
 		System.out.println("***********************************************************");
 		Catalog uneCatalog = Catalog.getSingleton();
@@ -46,61 +36,55 @@ public class TestTP04 {
 		uneCatalog.addRelationInfo(Personne);
 		uneCatalog.addRelationInfo(Etudiant);
 
-
-		
-		System.out.println("Nombre de relation dans la catalog : "+ uneCatalog.getNbRelation()+"\n");
-		System.out.println("Obtenir la relation Personne : "+ uneCatalog.getRelationInfo(Personne.getNomRelation())+"\n");
-		System.out.println("Obtenir la relation Etudiant : "+ uneCatalog.getRelationInfo(Etudiant.getNomRelation()));
+		System.out.println("Nombre de relation dans la catalog : " + uneCatalog.getNbRelation() + "\n");
+		System.out.println(
+				"Obtenir la relation Personne : " + uneCatalog.getRelationInfo(Personne.getNomRelation()) + "\n");
+		System.out.println("Obtenir la relation Etudiant : " + uneCatalog.getRelationInfo(Etudiant.getNomRelation()));
 		System.out.println("***********************************************************");
-		
-		
+
 		System.out.println("\n\n\nTest Record et de bufferSize");
 		System.out.println("***********************************************************");
-		
+
 		Record unRecord = new Record(Personne);
 		ArrayList<String> tuple1 = new ArrayList<>();
-		tuple1.add("Zhao");
-		tuple1.add("Julien");
+		tuple1.add("Lo");
+		tuple1.add("La");
 		tuple1.add("21");
 		tuple1.add("1.6");
 		unRecord.addTuple(tuple1);
 		System.out.println("INIT 1 : " + unRecord);
-		System.out.println("Le bufferSize de ce record est : " + unRecord.getWrittenSize() );
+		System.out.println("Le bufferSize de ce record est : " + unRecord.getWrittenSize());
 		ArrayList<String> tuple2 = new ArrayList<>();
-		tuple2.add("Maiche");
-		tuple2.add("Max");
+		tuple2.add("M");
+		tuple2.add("M");
 		tuple2.add("20");
 		tuple2.add("1.75");
 		Record deuxRecord = new Record(Personne);
 		deuxRecord.addTuple(tuple2);
 		System.out.println("INIT 2 : " + deuxRecord);
-		System.out.println("Le bufferSize de ce record est : " + deuxRecord.getWrittenSize() );
-		
-		
+		System.out.println("Le bufferSize de ce record est : " + deuxRecord.getWrittenSize());
+
 		System.out.println("\n\n\nTest de writeToBuffer et readFromBuffer : ");
 		System.out.println("***********************************************************");
-		//capacite d'un buff
+		// capacite d'un buff
 		ByteBuffer unBuffer = ByteBuffer.allocate(10000);
-       
-        
-        
-        unRecord.writeToBuffer(unBuffer, 20);
-        deuxRecord.writeToBuffer(unBuffer, 500);
-        
+
+		unRecord.writeToBuffer(unBuffer, 20);
+		deuxRecord.writeToBuffer(unBuffer, 500);
+
 		Record readUnRecord = new Record(Personne);
 		Record readDeuxRecord = new Record(Personne);
-		
+
 		readUnRecord.readFromBuffer(unBuffer, 20);
-        readDeuxRecord.readFromBuffer(unBuffer, 500);
-        
+		readDeuxRecord.readFromBuffer(unBuffer, 500);
+
 		System.out.println("INIT 3 : " + readUnRecord);
 		System.out.println("INIT 4 : " + readDeuxRecord);
-        
-        System.out.println("\nTest de getWrittenSize 1: " + unRecord.getWrittenSize());
-        System.out.println("\nTest de getWrittenSize 2: " + deuxRecord.getWrittenSize());
-        System.out.println("\nTest de getWrittenSize 3: " + readUnRecord.getWrittenSize());
-        System.out.println("\nTest de getWrittenSize 4: " + readDeuxRecord.getWrittenSize());
-        
+
+		System.out.println("\nTest de getWrittenSize 1: " + unRecord.getWrittenSize());
+		System.out.println("\nTest de getWrittenSize 2: " + deuxRecord.getWrittenSize());
+		System.out.println("\nTest de getWrittenSize 3: " + readUnRecord.getWrittenSize());
+		System.out.println("\nTest de getWrittenSize 4: " + readDeuxRecord.getWrittenSize());
 
 	}
 }
