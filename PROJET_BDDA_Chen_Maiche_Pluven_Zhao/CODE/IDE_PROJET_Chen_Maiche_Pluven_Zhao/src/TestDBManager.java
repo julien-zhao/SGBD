9f9914bee809838b011c4d7f4bdfdbbabc5e67fc
Moumix <max.maiche@gmail.com>
2022-12-08 12:49:16 +0100

    ok

--- PROJET_BDDA_Chen_Maiche_Pluven_Zhao/CODE/IDE_PROJET_Chen_Maiche_Pluven_Zhao/src/TestDBManager.java
+++ PROJET_BDDA_Chen_Maiche_Pluven_Zhao/CODE/IDE_PROJET_Chen_Maiche_Pluven_Zhao/src/TestDBManager.java
@@ -1,19 +1,17 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TestDBManager {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		DBParams.DBPath = "PROJET_BDDA_Chen_Maiche_Pluven_Zhao/DB/";
+		DBParams.DBPath = ".//..//..//DB//";
 		DBParams.pageSize = 4096;
 		DBParams.maxPagesPerFiles = 4;
 		DBParams.frameCount = 2;
 		DBManager dbm = DBManager.getSingleton();
 		dbm.init();
-		File f1= new File(DBParams.DBPath);
-		//deleteFolder(f1);
+		
 		System.out.println("******************************************************");
 		System.out.println("Début du DBManager:");
 		System.out.println("******************************************************");
